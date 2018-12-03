0de40147d52fd260955d7074c16e0d630d7d43cb
vetch <vetch97@gmail.com>
2018-12-03 01:45:40 +0100

    finally

--- src/com/kusnierz/agh/Presentation/ConsoleEmulator.java
+++ src/com/kusnierz/agh/Presentation/ConsoleEmulator.java
@@ -3,34 +3,47 @@ package com.kusnierz.agh.Presentation;
 import com.kusnierz.agh.Commands.*;
 import com.kusnierz.agh.Data.DataLoader;
 import com.kusnierz.agh.Data.Storage;
+import org.jline.reader.LineReader;
+import org.jline.reader.LineReaderBuilder;
+import org.jline.terminal.Terminal;
+import org.jline.terminal.TerminalBuilder;
 
 import java.io.IOException;
 import java.util.Scanner;
 
 public class ConsoleEmulator {
+    String prompt;
+    public ConsoleEmulator(String filePath,String prompt) throws IOException {
+        this.prompt=prompt;
+
+        Terminal terminal = TerminalBuilder.builder()
+                .system(true)
+                .attributes(null)
+                .build();
 
 
-    public static void main(String[] args) throws IOException {
-        Scanner reader = new Scanner(System.in);
+        LineReader lineReader = LineReaderBuilder.builder()
+                .terminal(terminal)
+                .build();
+
         String line;
-
         DataLoader loader=new DataLoader();
         Storage storage=loader.loadData("./Json");
-
-
-        while((line=reader.nextLine())!=null) {
-
+        terminal.writer().println("System ready for action");
+        while(true) {
+            line=lineReader.readLine(this.prompt);
+            if(line==null) continue;
             String[] command=line.split("\\s+",2);
             System.out.println(command[0]);
 
             if(command.length>1)
                 switch (command[0].toLowerCase()){
-                case "sig":
-                    System.out.println(new II().Command(command[1],storage));
-                    break;
-                case "reason":
-                    System.out.println(new III().Command(command[1],storage));
-                    break;
+                    case "sig":
+                        System.out.println(new II().Command(command[1],storage));
+                        break;
+                    case "reason":
+                        System.out.println(new III().Command(command[1],storage));
+                        break;
                     case "judge":
                         System.out.println(new V().Command(command[1],storage));
                         break;
@@ -51,14 +64,21 @@ public class ConsoleEmulator {
                         break;
 
                     default:
-                        System.out.println("Incorrect command");
-            }
-            else System.out.println("No arguments given");
+                        terminal.writer().println("Incorrect command");
+                }
+            else terminal.writer().println("No arguments given");
 
-            }
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        ConsoleEmulator konsola=new ConsoleEmulator("./Json","prompt> ");
+
+        //CmdLineParser console=new CmdLineParser();
 
         //2.4. sig
-        //command: getSig I UK 388/10 I UK 388/10
+        //command: sig I UK 388/10 I UK 388/10
         //3. reason
         // reason XVII AmC 1331/09
         //4. getSig Above ^
